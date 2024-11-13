9086947d293ad3c834b9a49aee88b917aca49cb8
Rajan66 <rajanmaharjan042@gmail.com>
2024-11-13 23:08:28 +0545

    feat: implemented post controller for activity

--- backend/src/main/java/com/example/backend/controller/ActivityController.java
+++ backend/src/main/java/com/example/backend/controller/ActivityController.java
@@ -1,9 +1,35 @@
 package com.example.backend.controller;
 
+import com.example.backend.dto.ActivityDto;
+import com.example.backend.dto.GlobalApiResponse;
+import com.example.backend.service.activity.ActivityService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(name = "/api/activity")
-public class ActivityController {
+public class ActivityController extends BaseController {
+
+    private final ActivityService activityService;
+
+    public ActivityController(ActivityService activityService) {
+        this.activityService = activityService;
+    }
+
+
+    @PostMapping("")
+    public ResponseEntity<GlobalApiResponse> createActivity(ActivityDto activityDto) {
+        ActivityDto savedActivityDto = activityService.save(activityDto);
+        if (savedActivityDto == null) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(failureResponse("Activity creation failed", null));
+        }
+        return ResponseEntity.ok(
+                successResponse("Activity created successfully", savedActivityDto)
+        );
+    }
 }
